b4a7fe06d500ddcf9f073aecf776ffd2f833eebe
Alexander Sachenko <sanyasach@mail.ru>
2025-03-16 13:39:54 +0300

    selenium driver

--- src/settings/utils.py
+++ src/settings/utils.py
@@ -9,8 +9,6 @@ def get_selenium_driver():
     options = ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    if config.PROD:
-        driver = Remote(f"http://{config.SELENIUM_HOST}:{config.SELENIUM_PORT}/wd/hub", options=options)
-    else:
-        driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
